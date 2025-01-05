2c15736c2a4a2f811f2352edd81e2f8b8905269e
Sara <sarailic431@gmail.com>
2025-01-05 19:09:07 +0100

    starting js of an app

--- src/main.js
+++ src/main.js
@@ -1,2 +1,31 @@
 "use strict";
 import "./style.css";
+
+const input = document.querySelector(".input-name");
+
+input.addEventListener("input", function (e) {
+  console.log(input.value);
+});
+
+const toDoCreator = function () {
+  let name = "";
+  let id = crypto.randomUUID();
+  let isDone = false;
+
+  const getName = () => name;
+
+  const setName = (value) => (name = value);
+
+  return { getName, setName };
+};
+
+const toDo = toDoCreator();
+
+const toDoManagerCreator = function () {
+  let toDos = [];
+
+  const addToDos = (toDo) => toDos.push(toDo);
+  const getToDos = () => toDos;
+
+  return { addToDos, getToDos };
+};
